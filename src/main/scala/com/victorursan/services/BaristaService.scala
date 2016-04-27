dea666b850cd3e2e5131df0524bb0ad5e133520f
Victor Ursan <victor.ursan@gmail.com>
2016-04-27 12:04:54 +0300

    changed the routes naming

--- src/main/scala/com/victorursan/services/BaristaService.scala
+++ src/main/scala/com/victorursan/services/BaristaService.scala
@@ -14,35 +14,38 @@ trait BaristaService extends BaseService {
   private val baristaController: BaristaController = new BaristaController
   baristaController.start()
 
-  protected val routes = pathPrefix("status") {
-    get {
-      log.info("/status executed")
-      complete(Status(Duration(ManagementFactory.getRuntimeMXBean.getUptime, MILLISECONDS).toString))
-    }
-  } ~ path("offers") {
-    get {
-      log.info("/offers executed")
-      onComplete(baristaController.offers()) {
-        case Success(listOffer) => complete(JsonTransformer getJsonArray listOffer prettyPrint)
-        case Failure(error)     => complete(error)
+  protected val routes =
+    path("status") {
+      get {
+        log.info("/status executed")
+        complete(Status(Duration(ManagementFactory.getRuntimeMXBean.getUptime, MILLISECONDS).toString))
       }
-    }
-  } ~ path("stop") {
-    log.info("/stop executed")
-    baristaController.stop()
-    complete {
-      System.exit(0)
-      "stop barista"
-    }
-  } ~ path("api" / "app") {
-    log.info("/api/app executed")
-    post {
-      entity(as[DockerEntity]) { dockerEntity =>
-        onComplete(baristaController.launchDockerEntity(dockerEntity)) {
-          case Success(result) => complete(result)
-          case Failure(error)  => complete(error)
+    } ~ path("stop") {
+      log.info("/stop executed")
+      baristaController.stop()
+      complete {
+        System.exit(0)
+        "stop barista"
+      }
+    } ~ pathPrefix("api") {
+      path("offers") {
+        get {
+          log.info("[GET] /api/offers executed")
+          onComplete(baristaController.offers()) {
+            case Success(listOffer) => complete(JsonTransformer getJsonArray listOffer prettyPrint)
+            case Failure(error)     => complete(error)
+          }
+        }
+      } ~ path("apps") {
+        post {
+          log.info("[POST] /api/apps executed")
+          entity(as[DockerEntity]) { dockerEntity =>
+            onComplete(baristaController.launchDockerEntity(dockerEntity)) {
+              case Success(result) => complete(result)
+              case Failure(error)  => complete(error)
+            }
+          }
         }
       }
     }
-  }
 }
