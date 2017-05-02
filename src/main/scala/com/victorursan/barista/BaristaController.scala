4db67473cba494cfe4584a27b1e493233670ad10
Victor Ursan <victor.ursan@gmail.com>
2017-05-02 10:12:50 +0300

    accidentally deleted method

--- src/main/scala/com/victorursan/barista/BaristaController.scala
+++ src/main/scala/com/victorursan/barista/BaristaController.scala
@@ -1,26 +1,11 @@
 package com.victorursan.barista
 
 import java.net.URI
-import java.util
-import java.util.{Optional, UUID}
+import java.util.UUID
 
-import com.google.protobuf.ByteString
-import com.mesosphere.mesos.rx.java.SinkOperations.sink
-import com.mesosphere.mesos.rx.java.protobuf.SchedulerCalls.decline
 import com.mesosphere.mesos.rx.java.util.UserAgentEntries
-import com.mesosphere.mesos.rx.java.{AwaitableSubscription, SinkOperation, SinkOperations}
-import com.victorursan.state.{Bean, DockerEntity}
+import com.victorursan.state.DockerEntity
 import com.victorursan.zookeeper.StateController
-import org.apache.mesos.v1.Protos
-import org.apache.mesos.v1.Protos._
-import org.apache.mesos.v1.scheduler.Protos.Call
-import org.apache.mesos.v1.scheduler.Protos.Call.{Acknowledge, Decline, Type}
-import org.slf4j.LoggerFactory
-import rx.lang.scala.Subject
-import rx.lang.scala.subjects.ReplaySubject
-import rx.subjects.SerializedSubject
-
-import scala.collection.JavaConverters._
 
 /**
   * Created by victor on 4/2/17.
@@ -30,12 +15,12 @@ class BaristaController {
   private val fwId = s"$fwName-${UUID.randomUUID}"
   private val mesosUri = URI.create("http://localhost:8000/mesos/api/v1/scheduler")
   private val role = "*"
-  //  val baristaCalls = new BaristaCalls
 
   def start(): Unit = {
     BaristaCalls.subscribe(mesosUri, fwName, 10, role, UserAgentEntries.literal("com.victorursan", "barista"), fwId)
   }
 
-
-
+  def launchDockerEntity(dockerEntity: DockerEntity): String = {
+    StateController.addToAccept(dockerEntity) mkString ","
+  }
 }
